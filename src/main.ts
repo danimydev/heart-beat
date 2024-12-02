f52d66d6b6382e30172e975d854c6befdf94c79d
Daniel Madrid <danimydev@gmail.com>
2024-12-01 23:33:49 -0500

    feat: setup actions and simple service managment

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,51 @@
-const kv = await Deno.openKv();
+import * as valibot from "valibot";
 
-const SERVICES_KEY = "services";
+import * as serviceRepository from "./service/repository.ts";
 
-type Service = {
-  name: string;
-  healthcheckUrl: string;
-};
+const serviceInputSchema = valibot.object({
+  name: valibot.string(),
+  healthcheckUrl: valibot.string(),
+});
 
-async function checkServiceHealthStatus(service: Service) {
+async function checkServiceHealthStatus(service: serviceRepository.Service) {
   const response = await fetch(service.healthcheckUrl);
   if (!response.ok) {
     return console.warn("Check service health status", service);
   }
-  return console.log("Healthcheck complete", service);
+  return console.log("Healthcheck complete", { service });
 }
+
+Deno.serve(async (request) => {
+  const { method, url } = request;
+  if (method !== "POST" || new URL(url).pathname !== "/") {
+    return new Response("Not Found", { status: 400 });
+  }
+  if (!request.body) {
+    return new Response("Bad Request", { status: 400 });
+  }
+  const body = await request.json();
+  const { success, output } = valibot.safeParse(serviceInputSchema, body);
+  if (!success) {
+    return new Response("Bad Request", { status: 400 });
+  }
+  const addedService = await serviceRepository.addService(output);
+  return new Response(
+    JSON.stringify({ message: "service added", addedService }),
+    {
+      status: 201,
+      headers: {
+        "content-type": "application/json",
+      },
+    },
+  );
+});
 
 Deno.cron(
   "Runs every 10 mins to check heartbeat",
   { minute: { every: 10 } },
   async () => {
     console.log("Running check heartbeat cron job...");
-    const servicesIterator = kv.list<Service>({ prefix: [SERVICES_KEY] });
-    const services: Service[] = [];
-    for await (const s of servicesIterator) {
-      services.push(s.value);
-    }
+    const services = await serviceRepository.getServices();
     const requests = services.map(checkServiceHealthStatus);
     await Promise.all(requests);
     console.log("Finished cron job execution");
---
+++ src/service/repository.ts
@@ -0,0 +1,31 @@
+import { NAMESPACE_DNS, v5 } from "@std/uuid";
+
+const kv = await Deno.openKv();
+
+const SERVICES_KEY = "services";
+
+export type Service = {
+  id: string;
+  name: string;
+  healthcheckUrl: string;
+};
+
+export async function getServices() {
+  const servicesIterator = kv.list<Service>({ prefix: [SERVICES_KEY] });
+  const services: Service[] = [];
+  for await (const s of servicesIterator) {
+    services.push(s.value);
+  }
+  return services;
+}
+
+export async function addService(service: Omit<Service, "id">) {
+  const data = new TextEncoder().encode(service.name);
+  const uuid = await v5.generate(NAMESPACE_DNS, data);
+  await kv.set([SERVICES_KEY, uuid], { id: uuid, ...service });
+  const addedService: Service = {
+    id: uuid,
+    ...service,
+  };
+  return addedService;
+}
